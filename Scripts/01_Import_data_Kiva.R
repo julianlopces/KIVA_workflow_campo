1ae45571a26ae6847aa2ae05cd9b8fb5d7a476a1
julianlopces <julianlopces@gmail.com>
2026-07-07 09:45:04 -0500

    eliminar pilotos

--- Scripts/01_Import_data_Kiva.R
+++ Scripts/01_Import_data_Kiva.R
@@ -81,6 +81,27 @@ data <- data %>%
   filter(username != "testkiva")
 
 data <- data %>%
+  filter(!KEY %in% c(
+    "uuid:75ab7815-3b0a-4e89-9833-bfcdd2444b13",
+    "uuid:0487f901-d48b-45ed-a2c8-d2e6153b3b9a",
+    "uuid:0e2ba87c-21d8-428a-8667-28c0940dd0e7",
+    "uuid:718f63d4-54f7-4774-a9fc-2902ae13eeeb",
+    "uuid:e291cf5c-a8ac-4bad-91cb-c99ecfac7ac6",
+    "uuid:5d501855-5af1-4f7d-8fd1-c5231fce6dfb",
+    "uuid:43d46bd1-1f95-4a5d-84e3-63a20a99045a",
+    "uuid:d1d9870e-e2c4-47d7-a4ad-4b13f1a128fe",
+    "uuid:4fb55b14-4c10-4770-b07a-e50cfc2ea50d",
+    "uuid:84a68e9e-5b9c-43d6-af96-08bafe256518",
+    "uuid:fc3c9f5c-5fd3-4197-a352-5abe21cecb9a",
+    "uuid:75aa1ac8-db64-4e2b-a650-c4673b3cf494",
+    "uuid:86519d33-1b37-4abf-ae53-31ba1d340944",
+    "uuid:2a6531d9-894f-4d70-bfdb-bcceedd82522",
+    "uuid:1d64a33d-9da7-43c8-91b4-f2e0622da29c",
+    "uuid:a6aab690-2edc-4878-bd8a-d4f7729251c7",
+    "uuid:718e3b13-bdc2-472f-88be-2c6158baa417"
+  ))
+
+data <- data %>%
   mutate(across(where(is.character), ~ na_if(str_trim(.x), "")),
          f2_ocup = as.numeric(f2_ocup))
 
